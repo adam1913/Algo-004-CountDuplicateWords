9c5022fe00dccc4c5fa92ae2286df9abee7a1e7d
Adam <adammartin1913@gmail.com>
2019-08-05 21:02:19 -0400

    adam new

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -4,34 +4,43 @@ package hbcu.stay.ready.algor;
 
 import java.util.*;
 
+
+
+
+
+
 public class Solution {
 
     public String countUniqueWords(String input) {
+        String view = "";
+
 
         Map<String, Integer> map = new HashMap<>();
-        Scanner sc = new Scanner(System.in);
-        String sentence = sc.nextLine();
 
-        String[] look = sentence.split(" ");
-        String word = look.toString();
 
-        for (String x : look) {
+        String[] look = input.split("\\.|\\!|\\s");
 
-            if (map.containsKey(x)) {
-                int count = map.get(look);
-                map.put(word, count + 1);
+
+        Integer count = 0;
+        for (String words : look) {
+
+
+            if (map.containsKey(words)) {
+
+                map.put(words, count++);
             } else {
-                map.put(word, 1);
+                map.put(words, 1);
 
 
             }
-            Set<String> keys = map.keySet();
 
-            TreeSet<String> sortedKeys = new TreeSet<>(keys);
+            
 
-            for (String str : sortedKeys);
 
+            view = "Seem" + map.keySet();
         }
-                return (word);
+        return view;
     }
 }
+
+
--- src/main/java/hbcu/stay/ready/algor/SolutionTest.java
+++ src/main/java/hbcu/stay/ready/algor/SolutionTest.java
@@ -4,21 +4,24 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class SolutionTest {
+public class SolutionTest  {
     Solution solution;
+
+
 
     @Before
     public void setup(){
         solution = new Solution();
     }
 
+
     @Test
     public void countUniqueWordsTest1(){
         String input = "Hello Java. Hello World. Hello Java!";
         String expected =   "The 3 unique words are:\n" +
-                            "Hello (Seen 3)\n" +
-                            "World (Seen 1)\n" +
-                            "Java (Seen 2)";
+                "Hello (Seen 3)\n" +
+                "World (Seen 1)\n" +
+                "Java (Seen 2)";
         String actual = solution.countUniqueWords(input);
         Assert.assertEquals(expected, actual);
     }
@@ -26,11 +29,12 @@ public class SolutionTest {
     @Test
     public void countUniqueWordsTest2(){
         String input = "that that is is that that is not is not is that it it is wutang";
-        String expected =   "The 4 unique words are:\n" +
-                            "that (Seen 5)\n" +
-                            "is (Seen 5)\n" +
-                            "not (Seen 2)\n"+
-                            "wutang (Seen 1)";
+        String expected ="The 5 unique words are:\n" +
+                "that (Seen 5)\n" +
+                "is (Seen 5)\n" +
+                "it (Seen 2)\n" +
+                "not (Seen 2)\n"+
+                "wutang (Seen 1)";
         String actual = solution.countUniqueWords(input);
         Assert.assertEquals(expected, actual);
     }
